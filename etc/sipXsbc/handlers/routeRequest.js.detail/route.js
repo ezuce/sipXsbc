525c892cec0ef7c99e15acbf5956c44b912659e8
Joegen Baclor <jbaclor@ezuce.com>
2012-09-13 13:50:30 +0800

    Added cisco retarget for out-of-dialog REFER.

--- etc/sipXsbc/handlers/routeRequest.js.detail/route.js
+++ etc/sipXsbc/handlers/routeRequest.js.detail/route.js
@@ -11,7 +11,26 @@ Route.prototype.isRoutable = function()
   if (typeof target == "undefined" || target.length == 0)
     return false;
   this.setTargetDomain(target);
-  
+
+
+  //
+  // Retarget Cisco out of dialog REFER
+  //
+  if (this.sipMessage.isRequest("REFER"))
+  {
+    log_info("Retarget Cisco out of dialog REFER");
+    var user = this.sipMessage.getRequestUriUser();
+    if (typeof user == "undefined" || user.length == 0)
+    {
+      user = this.sipMessage.getToUser();
+      if (typeof user != "undefined" && user.length > 0)
+      {
+        log_info("Retarget Cisco REFER to user " + user);
+        this.sipMessage.setRequestUriUser(user);
+      }
+    }
+  }
+
   //
   // Remove Cisco offending headers
   //
